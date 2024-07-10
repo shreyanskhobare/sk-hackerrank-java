7dfedb7b720f8e550c12ec538dfc4dadf1fadd26
Shreyans Khobare <khobareshreyans@gmail.com>
2024-07-10 18:04:58 +0100

    Initial Setup and Hourglass 2D Array

---
+++ src/main/java/com/sk/hackerrank/java/Hourglass2DArray.java
@@ -0,0 +1,37 @@
+/*
+Copyright [2024] Shreyans Dilip Khobare
+Proof of concept for Code Template
+*/
+package com.sk.hackerrank.java;
+
+import java.util.List;
+
+/**
+* @author - Shreyans Khobare
+*/
+public class Hourglass2DArray {
+
+	public int maximumHourglassValue(List<List<Integer>> arr) {
+		
+        int maximumValue = -999999;
+        for (int outer=0; outer<4; outer++) {
+            List<Integer> outerListRowOne = arr.get(outer); 
+            List<Integer> outerListRowTwo = arr.get(outer+1);
+            List<Integer> outerListRowThree = arr.get(outer+2);                   
+            for (int inner=0; inner<4; inner++) {
+                int rowOne = outerListRowOne.get(inner) + outerListRowOne.get(inner+1) + outerListRowOne.get(inner+2);
+                int rowTwo = outerListRowTwo.get(inner+1);
+                int rowThree = outerListRowThree.get(inner) + outerListRowThree.get(inner+1) + outerListRowThree.get(inner+2);
+               
+                if (maximumValue < (rowOne + rowTwo + rowThree)) {
+                    maximumValue = rowOne + rowTwo + rowThree;
+                }
+            }
+                    
+        }
+
+        return maximumValue;
+		
+	}
+	
+}
---
+++ src/test/java/com/sk/hackerrank/java/test/Hourglass2DArrayTest.java
@@ -0,0 +1,103 @@
+/*
+Copyright [2024] Shreyans Dilip Khobare
+Proof of concept for Code Template
+*/
+/**
+Copyright [2024] Shreyans Khobare
+Proof of concept for Copyright Statement
+ */
+package com.sk.hackerrank.java.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.sk.hackerrank.java.Hourglass2DArray;
+
+/**
+* @author - Shreyans Khobare
+*/
+/**
+ * 
+ */
+class Hourglass2DArrayTest {
+	
+	private Hourglass2DArray subjectUnderTest;
+	
+	@BeforeEach
+	public void setup() {
+		subjectUnderTest = new Hourglass2DArray();
+	}
+	
+	@Test
+	void testMaximumHourglassValue_1() {
+		List<List<Integer>> input = new ArrayList<List<Integer>>();
+		Integer[] oneArray = {1,1,1,0,0,0};
+		List<Integer> one = Arrays.asList(oneArray);
+		input.add(one);
+		
+		Integer[] twoArray = {0,1,0,0,0,0};
+		List<Integer> two = Arrays.asList(twoArray);
+		input.add(two);
+		
+		Integer[] threeArray = {1,1,1,0,0,0};
+		List<Integer> three = Arrays.asList(threeArray);
+		input.add(three);
+		
+		Integer[] fourArray = {0,9,2,-4,-4,0};
+		List<Integer> four = Arrays.asList(fourArray);
+		input.add(four);
+		
+		Integer[] fiveArray = {0,0,0,-2,0,0};
+		List<Integer> five = Arrays.asList(fiveArray);
+		input.add(five);
+		
+		Integer[] sixArray = {0,0,-1,-2,-4,0};
+		List<Integer> six = Arrays.asList(sixArray);
+		input.add(six);
+		
+		int response = subjectUnderTest.maximumHourglassValue(input);
+		assertEquals(response, 13);
+		
+		
+	}
+	
+	@Test
+	void testMaximumHourglassValue_2() {
+		List<List<Integer>> input = new ArrayList<List<Integer>>();
+		Integer[] oneArray = {1,1,1,0,0,0};
+		List<Integer> one = Arrays.asList(oneArray);
+		input.add(one);
+		
+		Integer[] twoArray = {0,1,0,0,0,0};
+		List<Integer> two = Arrays.asList(twoArray);
+		input.add(two);
+		
+		Integer[] threeArray = {1,1,1,0,0,0};
+		List<Integer> three = Arrays.asList(threeArray);
+		input.add(three);
+		
+		Integer[] fourArray = {0,0,2,4,4,0};
+		List<Integer> four = Arrays.asList(fourArray);
+		input.add(four);
+		
+		Integer[] fiveArray = {0,0,0,2,0,0};
+		List<Integer> five = Arrays.asList(fiveArray);
+		input.add(five);
+		
+		Integer[] sixArray = {0,0,1,2,4,0};
+		List<Integer> six = Arrays.asList(sixArray);
+		input.add(six);
+		
+		int response = subjectUnderTest.maximumHourglassValue(input);
+		assertEquals(response, 19);
+		
+		
+	}
+
+}
